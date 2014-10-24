4c7f2f555b3f4de4b5d2e92b87beab1ac747c48b
Tyler Wymer <twymer@gmail.com>
2014-10-24 10:00:40 -0400

    Implement basic method to get diff results for OT

--- public/assets/js/application.js
+++ public/assets/js/application.js
@@ -1,6 +1,8 @@
-var scheme   = "ws://";
-var uri      = scheme + window.document.location.host + "/";
-var ws       = new WebSocket(uri);
+var scheme = "ws://";
+var uri = scheme + window.document.location.host + "/";
+var ws = new WebSocket(uri);
+
+var currentText = "";
 
 ws.onmessage = function(message) {
   var data = JSON.parse(message.data);
@@ -14,14 +16,44 @@ ws.onmessage = function(message) {
   }
 };
 
+biggestStrLength = function (string1, string2) {
+  return string1.length > string2.length ? string1.length : string2.length
+}
+
+calculateDiff = function (newText, oldText) {
+  var start = end = null;
+  var longestTextLength = biggestStrLength(newText, oldText)
+
+  // iterate from start and end of strings to find the range
+  // that has been edited
+  for (var i = 0; i < longestTextLength; i++) {
+    if (start == null && newText[i] !== oldText[i]) {
+      start = i;
+    }
+    if (end == null && newText[newText.length - i] !== oldText[oldText.length - i]) {
+      end = newText.length - i + 1;
+    }
+  }
+
+  return {
+    'start': start,
+    'length': end - start,
+    'diff': newText.slice(start, end)
+  };
+}
+
 $("#input-text").on("change keyup paste", function () {
-  var name = $("#name-field")[0].value;
-  var text = $("#input-text")[0].value;
+  var textBox = $("#input-text")[0];
+  var nameBox = $("#name-field")[0];
+
+  var diffResults = calculateDiff(textBox.value, currentText);
+
+  currentText = textBox.value
 
   ws.send(JSON.stringify({
     action: "text_change",
-    username: name,
-    text: text
+    username: nameBox.value,
+    text: textBox.value
   }));
 });
 
